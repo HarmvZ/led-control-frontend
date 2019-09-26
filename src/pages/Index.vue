b471840c0574856725e69f7d6fe2d52789dff24a
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-09-27 01:12:58 +0200

    Add upcoming alarm to homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,16 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <div class="text-h5"> Upcoming alarm</div>
+    <q-card class="row">
+      <q-card-section>
+        <AlarmDetail v-if="alarm !== false" v-bind="alarm" :editable="false" />
+        <span v-else>No upcoming alarms found!</span>
+      </q-card-section>
+    </q-card>
+    <q-card class="row">
+      <q-card-section>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
@@ -8,7 +18,31 @@
 </style>
 
 <script>
+import AlarmDetail from 'components/alarm/AlarmDetail';
+
 export default {
   name: 'PageIndex',
+  components: { AlarmDetail },
+  data () {
+    return {
+      alarm: false,
+    };
+  },
+  methods: {
+    async getUpcomingAlarm () {
+      const url = '/api/alarms/first_upcoming_alarm';
+      const response = await this.request({
+        method: 'GET',
+        url,
+        responseType: 'json',
+      });
+      return response;
+    },
+  },
+  mounted () {
+    this.getUpcomingAlarm().then(data => {
+      this.alarm = data;
+    });
+  },
 };
 </script>
