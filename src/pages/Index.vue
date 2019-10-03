7974ebdc19672649ef6bc5ac01152be103a76797
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-10-03 22:57:07 +0200

    Improve index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,8 @@
 <template>
   <q-page class="items-center q-px-md">
-    <q-card class="col-12 bg-grey-9 text-center q-mt-md">
-      <q-card-section class="bg-primary text-white">
-        <div class="text-h5">Upcoming alarm</div>
-      </q-card-section>
-
-      <AlarmDetail
-        v-if="alarm !== false"
-        v-bind="alarm"
-        :editable="false"
-        style="margin-top: 0;"
-      />
-      <div v-else class="text-h6 justify-center q-pa-md">No upcoming alarms found!</div>
-    </q-card>
+    <router-link to="/alarms">
+      <UpcomingAlarm />
+    </router-link>
     <q-card class="col-12 bg-grey-9 text-center q-mt-md">
       <q-card-section class="bg-primary text-white">
         <div class="text-h5">Change color</div>
@@ -29,31 +19,15 @@
 
 <script>
 // import DarkCard from 'components/DarkCard';
-import AlarmDetail from 'components/alarm/AlarmDetail';
+import UpcomingAlarm from 'components/UpcomingAlarm';
 
 export default {
   name: 'PageIndex',
-  components: { AlarmDetail }, //, DarkCard },
+  components: { UpcomingAlarm }, //, DarkCard },
   data () {
     return {
       alarm: false,
     };
-  },
-  methods: {
-    async getUpcomingAlarm () {
-      const url = '/api/alarms/first_upcoming_alarm';
-      const response = await this.request({
-        method: 'GET',
-        url,
-        responseType: 'json',
-      });
-      return response;
-    },
-  },
-  mounted () {
-    this.getUpcomingAlarm().then(data => {
-      this.alarm = data;
-    });
   },
 };
 </script>
