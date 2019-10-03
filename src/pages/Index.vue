32610f7c62780ee0e320380af1fd9d36b5673718
Harm van Zeeland <harm.van.zeeland@gmail.com>
2019-10-04 00:04:25 +0200

    Improve index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,11 +5,32 @@
     </router-link>
     <q-card class="col-12 bg-grey-9 text-center q-mt-md">
       <q-card-section class="bg-primary text-white">
-        <div class="text-h5">Change color</div>
+        <div class="text-h6">Color</div>
       </q-card-section>
-      <q-card-section>
-        a
+      <div class="row items-center">
+        <ChangeColor
+          :transition="true"
+          :timestep="20"
+          :steps="100"
+          class="col-12"
+        />
+      </div>
+    </q-card>
+    <q-card class="col-12 bg-grey-9 text-center q-mt-md">
+      <q-card-section class="bg-primary text-white text-h6">
+        <div class="text-h6">
+          Animations
+        </div>
       </q-card-section>
+      <show-animation />
+    </q-card>
+    <q-card class="col-12 bg-grey-9 text-center q-mt-md">
+      <q-card-section class="bg-primary text-white text-h6">
+        <div class="text-h6">
+          Clock
+        </div>
+      </q-card-section>
+      <show-clock />
     </q-card>
   </q-page>
 </template>
@@ -20,10 +41,13 @@
 <script>
 // import DarkCard from 'components/DarkCard';
 import UpcomingAlarm from 'components/UpcomingAlarm';
+import ChangeColor from 'components/ChangeColor';
+import ShowClock from 'components/ShowClock';
+import ShowAnimation from 'components/ShowAnimation';
 
 export default {
   name: 'PageIndex',
-  components: { UpcomingAlarm }, //, DarkCard },
+  components: { UpcomingAlarm, ChangeColor, ShowClock, ShowAnimation }, //, DarkCard },
   data () {
     return {
       alarm: false,
